0cf6bf2cfdeeed8d4d69103df5e6c514dcc90851
Michael DeBerry <mdeberry@vitreousoft.com>
2026-01-24 20:34:08 -0500

    removing print guard

--- src/Overseer.Server/Automation/PrintGuard/IPrintGuardCameraStreamer.cs
+++
@@ -1,8 +0,0 @@
-namespace Overseer.Server.Automation.PrintGuard;
-
-public interface IPrintGuardCameraStreamer
-{
-  void Start(string url);
-  void Stop();
-  float[] GetProcessedFrame();
-}
--- src/Overseer.Tests/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using System.Text.Json;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Logging;
-global using Moq;
-global using Xunit;
